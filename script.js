0034a1878bd00fb3460eab07593317553ee071a8
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-04-30 14:54:29 +0300

    Async await applied for asynchronous operations

--- script.js
+++ script.js
@@ -1,84 +1,90 @@
 document.querySelector("#btnSearch").addEventListener("click", () => {
 
     let text = document.querySelector("#txtSearch").value;
+    document.querySelector("#details").style.opacity = 0;
     getCountry(text);
     
 }) 
 
-function getCountry(country) {
-    const request = new XMLHttpRequest();
+async function getCountry(country) {
 
-    request.open('GET', 'https://restcountries.com/v3.1/name/' + country);
-    request.send();
+    try {
 
-    request.addEventListener("load", function() {
-        const data = JSON.parse(this.responseText);
-        console.log(data);
+        const response = await fetch('https://restcountries.com/v3.1/name/' + country);
+        if(!response.ok) {
+            throw new Error("Country cannot be found. Please enter a valid text.");
+        };
+        const data = await response.json();
         renderCountry(data[0]);
 
-        const countries = data[0].borders.toString();
+        const countries = data[0].borders;
+        if(!countries) {
+            throw new Error("Neighbors do not exist for this country.");
+        };
+        const response2 = await fetch('https://restcountries.com/v3.1/alpha?codes=' + countries.toString());
+        const neighbors = await response2.json();
+        renderNeighbors(neighbors);
+        
+    }
+    catch(err) {
+        renderError(err);
+    }
+    //     .then((response) => {
+    //         if(!response.ok) {
+    //             throw new Error("Country cannot be found. Please enter a valid text.");
+    //         }
+    //         return response.json();
+    //     })
+    //     .then((data) => {
+    //         
+    //         
 
-        // Load Neighbor Contries
-        const req = new XMLHttpRequest();
-        req.open('GET', 'https://restcountries.com/v3.1/alpha?codes=' + countries)
-        req.send();
+    //         if(!countries) {
+    //             throw new Error("Neighbors do not exist for this country.");
+    //         }
 
-        req.addEventListener("load", function() {
-            const data = JSON.parse(this.responseText);
-            console.log(data);
-            renderNeighbors(data);
-        })
-    })
-
+    //         return 
+    //     })
+    //     .then(response => {
+    //         return response.json();
+    //     })
+    //     .then((data) => {
+    //         
+    //     })
+    //     .catch(err => renderError(err));
 }
 
 function renderCountry(data) {
+
+    document.querySelector("#country-details").innerHTML = "";
+    document.querySelector("#neighbors").innerHTML = "";
+
     let html = `
-        <div class="card-header">Search Results</div>
-        <div class="card-body">
+        <div class="col-4">
+            <img src="${data.flags.png}" alt="" class="img-fluid">
+        </div>
+        <div class="col-8">
+            <div class="card-title">${data.name.common}</div>
+            <hr>
             <div class="row">
-                <div class="col-4">
-                    <img src="${data.flags.png}" alt="" class="img-fluid">
-                </div>
-                <div class="col-8">
-                    <div class="card-title">${data.name.common}</div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-4">Population:</div>
-                        <div class="col-8">${(data.population / 1000000).toFixed(3)} Million</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4">Official Language:</div>
-                        <div class="col-8">${Object.values(data.languages)}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4">Capital:</div>
-                        <div class="col-8">${data.capital[0]}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4">Currency:</div>
-                        <div class="col-8">${Object.values(data.currencies)[0].name} (${Object.values(data.currencies)[0].symbol})</div>
-                    </div>
-                </div>
+                <div class="col-4">Population:</div>
+                <div class="col-8">${(data.population / 1000000).toFixed(3)} Million</div>
+            </div>
+            <div class="row">
+                <div class="col-4">Official Language:</div>
+                <div class="col-8">${Object.values(data.languages)}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">Capital:</div>
+                <div class="col-8">${data.capital[0]}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">Currency:</div>
+                <div class="col-8">${Object.values(data.currencies)[0].name} (${Object.values(data.currencies)[0].symbol})</div>
             </div>
         </div>
     `;
-    // const html = `
-    //     <div class="col-3">
-    //         <div class="card h-100">
-    //             <img src="${country.flags.png}" class="card-img-top">
-    //             <div class="card-body">
-    //                 <h5 class="card-title">${country.name.common}</h5>
-    //             </div>
-    //             <ul class="list-group list-group-flush">
-    //                 <li class="list-group-item">Population: ${(country.population / 1000000).toFixed(3)} Million</li>
-    //                 <li class="list-group-item">Capital: ${country.capital[0]} </li>
-    //                 <li class="list-group-item">Languages: ${Object.values(country.languages)} </li>
-    //             </ul>
-    //         </div>
-    //     </div>
-    // `;
-
+    document.querySelector("#details").style.opacity = 1;
     document.querySelector("#country-details").innerHTML = html;
 }
 
@@ -97,4 +103,18 @@ function renderNeighbors(data) {
         `
     }
     document.querySelector("#neighbors").innerHTML = html;
+}
+
+function renderError(err) {
+    const html = `
+        <div class="alert alert-danger">
+            ${err.message}
+        </div>
+    `;
+
+    setTimeout(function() {
+        document.querySelector("#errors").innerHTML = "";
+    }, 3000);
+
+    document.querySelector("#errors").innerHTML = html;
 }
